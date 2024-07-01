569e67ad57445e557f0cbcf91891a349ae294610
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-01 12:41:26 -0700

    feat(APP.JS): :lipstick: working on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { fetchData } from './db';
+import start from './assets/star-svg.svg'
 const App = () => {
   React.useEffect(() => {
     fetchData()
@@ -14,15 +15,20 @@ const App = () => {
   console.log(data[0].results); // Log the fetched data to the console
   // Fetch data and display it
   return (
-    <>
-      {movies && movies.map((movie) => (
-        <div className='' key={movie.id}>
-          <h2>{movie.title}</h2>
-          <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={` ${movie.title}  poster image`} />
-          <p>{movie.description}</p>
+    <div className='flex justify-center items-center flex-wrap '>
+      {movies && movies.map((movie, index) => (
+        <div className='flex flex-col justify-center items-center text-center w-48  m-4 border-2 border-solid-black rounded-md' key={movie.id}>
+
+          <img className='rounded-md w-full' src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={` ${movie.title}  poster image`} />
+          <p className='flex w-full p-2   justify-start items-center text-sm'>
+            <img className='h-4 w-4' src={start} alt='star icon' />
+            <span>{movie.vote_average}</span>
+          </p>
+          <h2>{`${index + 1}. ${movie.title}`}</h2>
+          {/* <p className=''>{movie.overview}</p> */}
         </div>
       ))}
-    </>
+    </div>
   )
 
 }
