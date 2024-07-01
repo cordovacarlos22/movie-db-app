e8b375f1357235ac5b0e063464105ca9d4d57334
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-01 12:11:10 -0700

    feat(APP.JS): :construction: importing DB and feching data that is being printed on the User front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { fetchData } from './db';
+const App = () => {
+  React.useEffect(() => {
+    fetchData()
+  }, []) // Fetch data only once when App component mounts
 
-function App() {
-  const [count, setCount] = useState(0)
+  let response = localStorage.getItem('movies');
+  let data = JSON.parse(response);
+  let movies = data[0].results
+  let moviesImages = `https://image.tmdb.org/${movies.backdrop_path
+    }`
 
+  console.log(data[0].results); // Log the fetched data to the console
+  // Fetch data and display it
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {movies && movies.map((movie) => (
+        <div className='' key={movie.id}>
+          <h2>{movie.title}</h2>
+          <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={` ${movie.title}  poster image`} />
+          <p>{movie.description}</p>
+        </div>
+      ))}
     </>
   )
+
 }
 
 export default App
