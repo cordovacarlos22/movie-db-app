adbba8dce221eecc09841fb0fe45f4375ca90f5a
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-07 13:47:57 -0700

    feat(components): :sparkles: added new component home  for home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NowPlaying from './NowPlaying';
+const Home = () => {
+  return (
+
+    <div className=' flex  flex-col '>
+     
+      <NowPlaying />
+    </div>
+
+  )
+}
+
+export default Home
