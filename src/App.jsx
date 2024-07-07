4d1ede5e93354481446f7b4d3872207818c9fbd1
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-06 22:24:39 -0700

    feat(APP.JS): :art: Improve code structure by importing Movie Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { fetchData } from './db';
 import start from './assets/star-svg.svg'
+import MovieCard from './components/MovieCard';
 const App = () => {
   React.useEffect(() => {
     fetchData()
-  }, []) // Fetch data only once when App component mounts
+  }, [fetchData()]) // Fetch data only once when App component mounts
 
   let response = localStorage.getItem('movies');
   let data = JSON.parse(response);
@@ -17,18 +18,14 @@ const App = () => {
   return (
     <div className='flex justify-center items-center flex-wrap '>
       {movies && movies.map((movie, index) => (
-        <div className='flex flex-col justify-center items-center text-center w-[178px] h-[400px]  m-4  ' key={movie.id}>
-
-          <img className=' w-full' src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={` ${movie.title}  poster image`} />
-          <div className='bg-black w-full h-full text-white'>
-            <p className='flex w-full p-2   justify-start items-center text-sm '>
-              <img className='h-4 w-4' src={start} alt='star icon' />
-              <span>{movie.vote_average}</span>
-            </p>
-            <h2 className='truncate'>{`${index + 1}. ${movie.title}`}</h2>
-            {/* <p className=''>{movie.overview}</p> */}
-          </div>
-        </div>
+        <>
+          <MovieCard
+            id={index + 1}
+            movieUrl={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            rating={movie.vote_average}
+            movieTitle={movie.title}
+          />
+        </>
       ))}
     </div>
   )
