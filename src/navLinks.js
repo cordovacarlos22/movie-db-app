12ea9bc500e15c1698e3a9c56680a5d597bef9b6
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-07 13:45:18 -0700

    feat(obj for navigation links):

---
+++ src/navLinks.js
@@ -0,0 +1,8 @@
+// navLinks.js
+export const navLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'Now Playing', path: '/now-playing' },
+  { name: 'Popular', path: '/popular' },
+  { name: 'Top Rated', path: '/top-rated' },
+  { name: 'Upcoming', path: '/upcoming' }
+];
