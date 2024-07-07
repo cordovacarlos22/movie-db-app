b7720df7d0b3513c848c8f7b0c564b66ece88469
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-06 22:23:50 -0700

    feat(components): :sparkles: add structure for movie cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
-const MovieCard = () => {
+import startIcon from '../assets/star-svg.svg'
+const MovieCard = ({ id, movieUrl, rating, movieTitle }) => {
   return (
-    <div>MovieCard</div>
+    <>
+      <section className='flex flex-col justify-center items-center text-center w-[178px] h-[400px]  m-4  ' key={id}>
+        <img className=' w-full' src={`${movieUrl}`} alt={` ${movieTitle}  poster image`} />
+        <div className='bg-black w-full h-full text-white'>
+          <p className='flex w-full p-2   justify-start items-center text-sm '>
+            <img className='h-4 w-4' src={startIcon} alt='star icon' />
+            <span>{`${rating}`}</span>
+          </p>
+          <h2 className='truncate'>{`${id}. ${movieTitle}`}</h2>
+          {/* <p className=''>{movie.overview}</p> */}
+        </div>
+      </section>
+    </>
   )
 }
 
