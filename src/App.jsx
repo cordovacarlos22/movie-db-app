bdeccd8ee2e4f951f28b251318a8386530bb2e10
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-08 07:19:18 -0700

    feat(APP.JS): :sparkles: imported Popular Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './components/Nav'
 import Home from './components/Home';
 import NowPlaying from './components/NowPlaying';
+import Popular from './components/Popular';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/now-playing" element={<NowPlaying />} />
+        <Route exact path="/popular" element={<Popular />} />
       </Routes>
     </Router>
   )
