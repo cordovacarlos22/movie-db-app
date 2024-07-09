bf4b6daa6cbcb2dbf776fcb61e0150ee1e07a13d
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-09 06:47:25 -0700

    feat(components): :sparkles: added span for release data

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 
 import startIcon from '../assets/star-svg.svg'
-const MovieCard = ({ id, movieUrl, rating, movieTitle, children }) => {
+const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate }) => {
   return (
-    <div className=' flex-1  flex  flex-wrap justify-center items-center border-2 border-red-200'>
-      <section className='flex flex-col justify-center items-center text-center w-[178px] h-[400px]  m-2 ' id={id}>
+    <div className=' flex-1  flex  flex-wrap justify-center items-center border-2 border-red-200   '>
+      <section className='flex flex-col justify-center items-center text-center w-[225px]    ' id={id}>
         <img className=' w-full' src={`${movieUrl}`} alt={` ${movieTitle}  poster image`} />
         <div className='bg-black w-full h-full text-white'>
           <p className='flex w-full p-2   justify-start items-center text-sm '>
+            Rating:
             <img className='h-4 w-4' src={startIcon} alt='star icon' />
-            <span>{`${rating}`}</span>
+            <span className='flex w-full p-2   justify-start items-center text-s'>{`${rating}`}</span>
           </p>
-          <h2 className='truncate'>{`${id}. ${movieTitle}`}</h2>
+          <span>release: {`${releaseDate}` }</span>
+          <h2 className='truncate'>{`${index}. ${movieTitle}`}</h2>
           {/* <p className=''>{movie.overview}</p> */}
         </div>
       </section>
