c327f28baf0bcaa0a7ffb31d0a156d9a252f94f8
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-09 06:53:27 -0700

    feat(DB): :art: REMOVED CONSOLE LOG AND CLEAN UP CODE

--- src/db.js
+++ src/db.js
@@ -1,12 +1,7 @@
 export const fetchData = async (category, page = 1) => {
 
-  const apiKey = import.meta.env.VITE_API_KEY;
+  // const apiKey = import.meta.env.VITE_API_KEY;
   const apiReadAccessToken = import.meta.env.VITE_APIREADACCESSTOKEN;
-  const mbdoApiKey = import.meta.env.VITE_MBDOAPIKEY;
-
-  console.log('API Key:', apiKey); // Should log the value from .env
-  console.log('API Read Access Token:', apiReadAccessToken); // Should log the value from .env
-  console.log('MBDO API Key:', mbdoApiKey); // Should log the value from .env 
 
   // ! Fetch data from the API endpoint
   const url = `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=${page}`;
@@ -22,10 +17,11 @@ export const fetchData = async (category, page = 1) => {
     let response = await fetch(url, options);
     let data = await response.json();
     //? Log the fetched data to the console
-    console.log(data);
+    // console.log(data);
     localStorage.setItem(`${category}`, JSON.stringify([data]))
   } catch (error) {
     console.error(error);
+    alert(error);
   }
 };
 
@@ -34,9 +30,3 @@ export const fetchData = async (category, page = 1) => {
 // // {
 // popular, now_playing, top_rated, upcoming
 // // }
-
-
-
-
-
-
