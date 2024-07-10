7eb1743f96ccbe53123ff5804235d12286342cd1
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-10 10:14:24 -0700

    feat(APP.JS): :sparkles: imported new components and created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Home from './components/Home';
-import NowPlaying from './components/NowPlaying';
-import Popular from './components/Popular';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NowPlayingMovies from './components/NowPlayingMovies';
+import PopularMovies from './components/PopularMovies';
+import TopRatedMovies from './components/TopRatedMovies';
+import UpComingMovies from './components/UpComingMovies';
+
 const App = () => {
   return (
     <Router>
       <Nav />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/now-playing" element={<NowPlaying />} />
-        <Route exact path="/popular" element={<Popular />} />
+        <Route exact path="/now-playing" element={<NowPlayingMovies />} />
+        <Route exact path="/popular" element={<PopularMovies />} />
+        <Route exact path="/top-rated" element={<TopRatedMovies />} />
+        <Route exact path="/upcoming" element={<UpComingMovies />} />
       </Routes>
     </Router>
   )
