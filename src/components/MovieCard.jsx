49f67df8ea299c870faeba6f057c722bb1244ea6
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-10 10:20:09 -0700

    feat(components): :lipstick: UPADTED STYLES

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,17 +2,17 @@
 import startIcon from '../assets/star-svg.svg'
 const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate }) => {
   return (
-    <div className=' flex-1  flex  flex-wrap justify-center items-center border-2 border-red-200   '>
+    <div className=' flex-1  flex  flex-wrap justify-center items-center   '>
       <section className='flex flex-col justify-center items-center text-center w-[225px]    ' id={id}>
-        <img className=' w-full' src={`${movieUrl}`} alt={` ${movieTitle}  poster image`} />
-        <div className='bg-black w-full h-full text-white'>
+        <img className=' w-full rounded-t' src={`${movieUrl}`} alt={` ${movieTitle}  poster image`} />
+        <div className='bg-black w-full h-full text-white rounded-b'>
           <p className='flex w-full p-2   justify-start items-center text-sm '>
+            <img className='h-6 w-6' src={startIcon} alt='star icon' />
             Rating:
-            <img className='h-4 w-4' src={startIcon} alt='star icon' />
             <span className='flex w-full p-2   justify-start items-center text-s'>{`${rating}`}</span>
           </p>
           <span>release: {`${releaseDate}` }</span>
-          <h2 className='truncate'>{`${index}. ${movieTitle}`}</h2>
+          <h2 className='truncate p-2'>{`${index}. ${movieTitle}`}</h2>
           {/* <p className=''>{movie.overview}</p> */}
         </div>
       </section>
