a003837ed8ccc537ea10b46aea4da9f50e8edaea
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-15 07:58:29 -0700

    feat(components): :sparkles: add card on hover that displays description about the movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 
 import startIcon from '../assets/star-svg.svg'
-const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate }) => {
+const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate, overview }) => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -10,21 +10,30 @@ const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate }) => 
       year: 'numeric'
     }).format(date);
   };
-  
+
   return (
-    <div className=' flex-1  flex  flex-wrap justify-center items-center   '>
-      <section className='flex flex-col justify-center items-center text-center w-[225px]    ' id={id}>
+    <div className='   flex  flex-wrap justify-center items-center  relative '>
+      <section className='flex flex-col justify-center items-center text-center w-[225px] relative    ' id={id}>
         <img className=' w-full rounded-t' src={`${movieUrl}`} alt={` ${movieTitle}  poster image`} />
-        <div className='bg-black w-full h-full text-white rounded-b'>
+        <div className='bg-gray-800 w-full h-full text-white rounded-b'>
           <p className='flex w-full p-2   justify-start items-center text-sm '>
             <img className='h-6 w-6' src={startIcon} alt='star icon' />
             Rating:
             <span className='flex w-full p-2   justify-start items-center text-s'>{`${rating}`}</span>
           </p>
-          <h2 className='truncate p-2'>{`${index}. ${movieTitle}`}</h2>
+          <h2 className='truncate flex flex-wrap p-2'>{`${index}. ${movieTitle}`}</h2>
           <span> {formatDate(`${releaseDate}`)}</span>
-          {/* <p className=''>{movie.overview}</p> */}
+          
         </div>
+      </section>
+      <section className='absolute top-0 left-0 w-full  h-full bg-blue-500 text-white p-2 rounded opacity-0 hover:opacity-100 flex flex-col items-center justify-center text-center '>
+        <h2 className='text-ellipsis font-bold p-2'>{index}. {movieTitle}</h2>
+        <p className='flex items-center text-sm'>
+          <img className='h-6 w-6' src={startIcon} alt='star icon' />
+          Rating:
+          <span className='ml-1'>{rating}</span>
+        </p>
+        <p className='text-sm text-wrap overflow-scroll ' >{overview}</p>
       </section>
     </div>
   )
