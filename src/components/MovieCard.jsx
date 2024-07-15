21055172e2ecd5b2a8e1f26e9125afa7424b67e7
carloscordova.dev <cordovamanuel22@icloud.com>
2024-07-15 07:16:25 -0700

    feat(components): :boom: introduced format Data function

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,16 @@
 
 import startIcon from '../assets/star-svg.svg'
 const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate }) => {
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('en-US', {
+      month: 'short',
+      day: '2-digit',
+      year: 'numeric'
+    }).format(date);
+  };
+  
   return (
     <div className=' flex-1  flex  flex-wrap justify-center items-center   '>
       <section className='flex flex-col justify-center items-center text-center w-[225px]    ' id={id}>
@@ -11,8 +21,8 @@ const MovieCard = ({ id, index, movieUrl, rating, movieTitle, releaseDate }) => 
             Rating:
             <span className='flex w-full p-2   justify-start items-center text-s'>{`${rating}`}</span>
           </p>
-          <span>release: {`${releaseDate}` }</span>
           <h2 className='truncate p-2'>{`${index}. ${movieTitle}`}</h2>
+          <span> {formatDate(`${releaseDate}`)}</span>
           {/* <p className=''>{movie.overview}</p> */}
         </div>
       </section>
